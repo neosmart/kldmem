0e4ea83ca02a451cf49a890347ad8478a396d1f9
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-10-26 16:39:12 -0500

    Support --human/-h for human sizes

--- main.c
+++ main.c
@@ -4,15 +4,37 @@
 #include <stdio.h>
 #include <string.h>
 
-void show(struct kld_file_stat *stat) {
+#if !defined(__cplusplus)
+#define bool uint8_t
+#define false 0
+#define true 1
+#endif
+
+void show(struct kld_file_stat *stat, bool human) {
 	printf("%s\n", stat->name);
-	printf("* Memory usage: %lu\n", stat->size);
+
+	if (!human) {
+		printf("* Memory usage: %lu\n", stat->size);
+	} else if (stat->size < 1024) {
+		printf("* Memory usage: %lub\n", stat->size);
+	} else if (stat-> size < 1024 * 1024) {
+		printf("* Memory usage: %.1fk\n", stat->size / 1024.);
+	} else {
+		printf("* Memory usage: %.1fm\n", stat->size / 1024. / 1024.);
+	}
+
 	printf("* Modules referenced: %d\n", stat->refs);
 	printf("* Path: %s\n", stat->pathname);
 	printf("\n");
 }
 
 int main(int argc, const char *argv[]) {
+	bool human = false;
+	if (argc > 1 && (strcmp(argv[1], "-h") == 0 ||
+			 strcmp(argv[1], "--human") == 0)) {
+		human = true;
+	}
+
 	for (int kldId = kldnext(0); kldId > 0; kldId = kldnext(kldId)) {
 		struct kld_file_stat stat = {
 			.version = sizeof(struct kld_file_stat),
@@ -25,6 +47,6 @@ int main(int argc, const char *argv[]) {
 			return errno;
 		}
 
-		show(&stat);
+		show(&stat, human);
 	}
 }
